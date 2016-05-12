8c5fb427744b000b83aa948d95fa0f9cc25c4d25
Aubin <aubin@fstck.co>
2016-05-12 11:14:22 +0200

    Add Bcc for email using Recipients method @see http://dev.mailjet.com/guides/#sending-a-basic-email

--- src/MailjetTransport.php
+++ src/MailjetTransport.php
@@ -17,28 +17,32 @@ class MailjetTransport extends Transport
     public function send(Swift_Mime_Message $message, &$failedRecipients = null)
     {
         $this->beforeSendPerformed($message);
-        
+
         $email = $this->buildEmailData($message);
-        
+
         return $this->client->post(\Mailjet\Resources::$Email, ['body' => $email]);
     }
-    
+
     protected function buildEmailData(Swift_Mime_Message $message)
     {
         $data = [];
-        
+
         foreach ($message->getFrom() as $address => $display) {
             $data['FromEmail'] = $address;
             $data['FromName'] = $display != null ? $display : $address;
             break;
         }
-        
+
         foreach ($message->getTo() as $address => $display) {
             $data['Recipients'][] = ['Email' => $address, 'Name' => $display];
         }
-        
+
+        foreach($message->getBcc() as $address => $display) {
+            $data['Recipients'][] = ['Email' => $address, 'Name' => $display];
+        }
+
         $data['Subject'] = $message->getSubject();
-        
+
         if ($message->getContentType() == 'multipart/alternative') {
             $data['Html-Part'] = $message->getBody();
             foreach ($message->getChildren() as $child) {
@@ -53,9 +57,9 @@ class MailjetTransport extends Transport
         elseif ($message->getContentType() == 'text/plain') {
             $data['Text-Part'] = $message->getBody();
         }
-        
+
         // TODO: Attachments
-        
+
         return $data;
     }
 }
